55f7c2b2db000ecb5fb0731c0356241413aaabe5
Jimmy McDermott <jimmymac_205@yahoo.com>
2014-08-27 20:24:23 -0500

    Initial Commit

---
+++ Hello World/Hello World/Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Jim McDermott on 8/27/14.
+//  Copyright (c) 2014 jdmcd studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
