8196349c27146e27beacea120b4a9bedcdf3434e
Jimmy McDermott <jimmymac_205@yahoo.com>
2014-08-28 21:38:09 -0500

    Moved challenge one to folder

---
+++ challengeOne.playground/section-1.swift
@@ -0,0 +1,41 @@
+//challengeOne.playground
+
+//MVP
+
+var lastName:String = "McDermott"
+var titleName:String = "Mr."
+var sentence:String = "Hello, "+titleName+" "+lastName
+println(sentence)
+
+//stretch 1
+
+//defines names
+var playerOne:String = "Bobby "
+var playerTwo:String = "Jimmy "
+var playerThree:String = "Katie"
+var playerFour:String = "Abby"
+//assigns teams
+var teamOne = playerOne+"and "+playerFour
+var teamTwo = playerTwo+"and "+playerThree
+
+
+//stretch 2
+
+//constants
+let name:String = "iPhone"
+let maker:String = "Apple"
+let yearIntroduced:Int = 2007
+let operatingSystem:String = "iOS"
+//variables
+var latestModel:String = "5s"
+var latestUpdate:Double = 7.1
+
+//stretch 2 sentences
+println("\(name) was introduced in \(yearIntroduced).")
+println("Made by \(maker).")
+println("The operating system is \(operatingSystem).")
+println("The latest model is the iPhone \(latestModel).")
+println("The latest update to the iPhone \(latestModel) is \(operatingSystem) \(latestUpdate)")
+
+
+//end of program
