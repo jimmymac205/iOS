f253405fa0fd03f03e3709da02c344b0aef8d9c3
Jimmy McDermott <jimmymac_205@yahoo.com>
2014-09-01 18:14:58 -0500

    Added more views

--- Pro Cast/Pro Cast/ViewController.swift
+++ Pro Cast/Pro Cast/ViewController.swift
@@ -9,17 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var test: UIWebView!
                             
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        let url = NSURL(string: "http://rshankar.com")
+        let request = NSURLRequest(URL: url)
+        test.loadRequest(request)
+    
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+   
 }
 
